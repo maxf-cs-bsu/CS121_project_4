c621acae3580107b13ec2ff794152b299753ff47
maxf-cs-bsu <maxcfleck@gmail.com>
2026-09-19 02:35:04 +0000

    updated horsey.cpp

--- project4/horsey.cpp
+++ project4/horsey.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 
 const int TRACK=15;
-const int SIZE=5;
+const int AMOUNT_HORSES=5;
 
 void advance(int horseNum, int* horses){
   int coinFlip = rand() % 2; 
@@ -30,12 +30,19 @@ bool isWinner(int horseNum, int* horses){
 int main(){
   srand(time(NULL));
 
-  int horses[SIZE] = {0};
+  int horses[AMOUNT_HORSES] = {0, 0, 0, 0, 0};
   bool keepGoing = true;
 
-  for (int i = 0; i < 5; i++){
-    printLane(horses[i], horses);
-    advance(horses[i], horses);
+  while (keepGoing){
+    for (int i = 0; i < AMOUNT_HORSES; i++){
+      printLane(horses[i], horses);
+      advance(horses[i], horses);
+      if (isWinner(horses[i], horses)){
+        keepGoing = false;
+      }
+    }
+    std::cout << "Press Enter for next turn: ";
+    std::cin.get();
   }
   return (0);
 } // end of main
